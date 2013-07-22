4dc33db2e741bd87c941b94725286dd955c03404
Johnnyscriptus <mostmodernist@gmail.com>
2013-07-22 14:55:49 -0700

    gettin busy

--- sourceCap.js
+++ sourceCap.js
@@ -1,50 +1,107 @@
 var fs = require('fs');
-var css = fs.readFileSync('../flat-radio-field/assets/flatfield.css', 'utf8');
-var hyperquest = require('hyperquest');
-var url = require('url');
-var ssn = require('./synth-server-node');
+var css = fs.readFileSync('../css/flatfield.css', 'utf8');
 var html = require('hyperscript');
 var touchdown = require('touchdown');
+var appendCSS = require('../uxer/appendCSS');
+var hover = require('mouse-around')
 
+module.exports = function(sourceEvents){
 
-module.exports = function(context){
+    var change = sourceEvents.change;
+    var click = sourceEvents.click;
+    
+    appendCSS(css, 'uxer-flatfield')
 
-    var es = document.getElementById('uxer-flatfield-style');
-
-    if(es){
-	es.parentNode.insertBefore(makeStyle(css), es.nextSibling)
-    }
-    else{
-	document.head.insertBefore(makeStyle(css), document.head.childNodes[0]);
-    }
-
-    var _fieldset = html('fieldset.uxer-flat-fieldset');
-    var _legend =  html('legend.uxer-flatfield-legend', 'ADD A NEW SOURCE')
+    var _fieldset = html('fieldset.uxer-flat-fieldset.sourceCapture');
+    var _legend =  html('legend.uxer-flatfield-legend', '+ ADD AUDIO SOURCE +')
 
     _fieldset.appendChild(_legend);
 
-    var ytextLabel = html('div.uxer-flatfield-label');
+    var options = html('div.sourceCapOpts')
+    _fieldset.appendChild(options);
+  
+    var mode = html('div.sourceCapMode')
+    _fieldset.appendChild(mode);
+ 
+    var samplesLabel = html('label.uxer-flatfield-label.sourceCapOptsLabel');
+    var samples = html('button.uxer-flat-button', 
+		       {textContent: 'LOAD SAMPLES',
+			name: 'samplebank',
+			ontouchdown: function(e){click.call(samples, e)}
+		       });
+
+    hover(_fieldset, function(evt, node, pos, start, stop){ })
+    touchdown.start(samples);
+    samplesLabel.appendChild(samples);
+    options.appendChild(samplesLabel);
+
+
+    var ytextLabel = html('div.uxer-flatfield-label.sourceCapOptsLabel');
     var ytext = html('input.uxer-flatfield-input', 
 		     {value: 'http://www.youtube.com/watch?v=1OixqPThDNE', 
 		      placeholder: 'PASTE YOUTUBE LINK',
 		      type: 'url',
-		      name: 'url'
+		      name: 'sourceURL'
 		     });
-
-    var ytextButton = html('button.uxer-flat-button',
-			   {textContent: 'CAPTURE INTERNET AUDIO', 
+    var ytextModeButton = html('button.uxer-flat-button',
+			   {textContent: 'USE INTERNET AUDIO', 
 			    ontouchdown: function(e){e.preventDefault();click.call(ytext, e)}
 			    });
 
-    touchdown.start(ytextButton);
-    ytextLabel.appendChild(ytext);
-    ytextLabel.appendChild(ytextButton);
-    _fieldset.appendChild(ytextLabel);
+    var ytextModeInfo = html('p.infoHelp', 
+      [ 'You can load audio from almost anywhere on the internet: youtube, vimeo, soundcloud, bandcamp, synth.fm, or a direct link to an audio or video file. Soon you\'ll be able to search those same sources for multiple results, and even capture live ogg/mp3 streams.' 
+      , html('br')
+      , html('br')
+      , html('h3', 'Examples:')
+      , html('ul', [
+	  html('li', 'http://www.youtube.com/watch?v=1OixqPThDNE'),
+	  html('li', 'http://synth.fm/UdP4RuL3z')
+      ])
+      , html('h3', 'Supported audio File Types:')
+      , html('ul', ['mp3', 'wav', 'ogg', 'aac'].map(function(k){
+	    return html('li', k)
+	}))
+      ]) 
 
-    var fileCapLabel = html('label.uxer-flatfield-label');
+    var fileModeInfo = html('p.infoHelp', [
+	html('h3', 'Load one or more files from your computer'),
+	html('h3', 'Supported File Types:'),
+	html('ul', ['mp3', 'wav', 'ogg', 'aac'].map(function(k){
+	    return html('li', k)
+	}))
+    ]) 
+
+    var ytextMode = html('div#sourceURL');
+    var fileMode = html('div#soureceFile');
+    var micLineMode = html('div#sourceMicLine');
+    var sampleMode = html('div#sourceSample');
+
+    mode.appendChild(ytextMode);
+    mode.appendChild(fileMode);
+    mode.appendChild(micLineMode);
+    mode.appendChild(sampleMode);
+
+    fileMode.appendChild(fileModeInfo);
+
+    ytextLabel.appendChild(ytext);
+    ytextMode.appendChild(ytextLabel)
+    ytextMode.appendChild(ytextModeInfo)
+
+    var ytextOptionLabel = html('div.uxer-flatfield-label.sourceCapOptsLabel');
+    var ytextOptionButton = html('button.uxer-flat-button',
+			   {textContent: 'USE INTERNET AUDIO', 
+			    ontouchdown: function(e){e.preventDefault();click.call(ytext, e)}
+			    });
+
+//    touchdown.start(ytextButton);
+//    ytextLabel.appendChild(ytext);
+    ytextOptionLabel.appendChild(ytextOptionButton);
+    options.appendChild(ytextOptionLabel);
+
+    var fileCapLabel = html('label.uxer-flatfield-label.sourceCapOptsLabel');
     var fileCap = html('input.uxer-flatfield-input', {type: 'file', name: 'file', onchange: change});
     var fileCapButton = html('button.uxer-flat-button',
-			   {textContent: 'OPEN LOCAL AUDIO FILE', 
+			   {textContent: 'OPEN A LOCAL AUDIO FILE', 
 			    name: 'file'
 			    });
 
@@ -52,20 +109,20 @@ module.exports = function(context){
     touchdown.start(fileCapButton);
     fileCapLabel.appendChild(fileCap);
     fileCapLabel.appendChild(fileCapButton);
-    _fieldset.appendChild(fileCapLabel);
+    options.appendChild(fileCapLabel);
 
 
-    var miclineLabel = html('label.uxer-flatfield-label');
+    var miclineLabel = html('label.uxer-flatfield-label.sourceCapOptsLabel');
     var micline = html('button.uxer-flat-button', 
-		       {textContent: 'CAPTURE MIC/LINE IN',
-			name: 'line',
+		       {textContent: 'CAPTURE MIC / LINE-IN',
+			name: 'line', 
 			ontouchdown: function(e){click.call(micline, e)}
 		       });
 
 
     touchdown.start(micline);
     miclineLabel.appendChild(micline);
-    _fieldset.appendChild(miclineLabel);
+    options.appendChild(miclineLabel);
 
     var playbuttonLabel = html('label.uxer-flatfield-label');
     var playbutton = html('button.uxer-flat-button', 
@@ -74,134 +131,16 @@ module.exports = function(context){
 			ontouchdown: function(e){click.call(playbutton, e)}
 		       });
 
-    touchdown.start(playbutton);
-    playbuttonLabel.appendChild(playbutton);
-    _fieldset.appendChild(playbuttonLabel);
+ //   touchdown.start(playbutton);
+ //   playbuttonLabel.appendChild(playbutton);
+ //   _fieldset.appendChild(playbuttonLabel);
     
     document.body.appendChild(_fieldset)
 
     var fset = _fieldset;
 
-
     return _fieldset;
 
-    function change(e){
-	if(this.name == 'url') return
-	if(this.name == 'file'){
-
-	    if(this.files[0].size > 1920000){
-		var audio = new Audio();
-		audio.src = window.URL.createObjectURL(this.files[0]);
-		audio.addEventListener('canplay', function(){
-		    var source = context.createMediaElementSource(audio);
-		    var evt = new CustomEvent('sourceCap', {bubbles: true, detail: source});
-//		    window.dispatchEvent(evt)
-		    fset.dispatchEvent(evt);
-		}, true)
-	    }
-	    else{
-		var reader = new FileReader();
-		reader.onload = function(e){
-		    var buffer = e.target.result;
-		    var source = context.createBufferSource();
-		    try{
-			var buf = context.createBuffer(buffer, true);
-			source.buffer = buf;
-			var evt = new CustomEvent('sourceCap', {bubbles: true, detail: source});
-			fset.dispatchEvent(evt);
-		    } 
-		    catch(e){
-			alert('Error: Probably an unsupported file type.')
-			// and this is where you would decode it with Aurora ...
-		    }
-		};
-		try{
-		    reader.readAsArrayBuffer(this.files[0]);
-		} 
-		catch(e){
-		    alert('Error: Probably an unsupported file type.')
-		}
-	    }
-	}
-    }
-
-    function click(e){
-
-	if(this.name == 'url'){
-	    e.preventDefault();
-	    var uri = this.value;
-	    if(uri.length){
-		var parsed = url.parse(uri, true);
-	
-		if(((parsed.slashes || parsed.protocol) && (parsed.hostname.match('youtube.com'))) || 
-		   (parsed.pathname && parsed.pathname.match('youtube.com'))) {
-
-		    if(Modernizr.touch){
-
-			var source = ssn(master, uri, function(err, source){
-			    var evt = new CustomEvent('sourceCap', {bubbles: true, detail: source});
-			    fset.dispatchEvent(evt);
-			});
-
-		    }
-
-		    else{
-
-			// this is not gonna happen...
-			var id = parsed.hostname === 'youtu.be' 
-			    ? parsed.pathname.slice(1) : parsed.query.v;
-
-			hyperquest('http://localhost:11002/get_info?'+uri, function(err, res){
-			    res.on('data', function(data){
-
-				var URI = data;
-				var video = document.createElement('video');
-				video.src = URI;
-				video.addEventListener('loadstart', function(){
-			
-				    var source = context.createMediaElementSource(video);
-				    var evt = new CustomEvent('sourceCap', {bubbles: true, detail: source});
-				    fset.dispatchEvent(evt);
-
-				});
-
-			    })
-			})
-
-		    }
-		}
-		else console.log('nup');
-	    }
-	}
-	if(this.name == 'file'){
-	}
-	if(this.name == 'line'){ // getUserMedia
-	    navigator.webkitGetUserMedia({audio: true, video: false}, function(stream){
-		var source = context.createMediaStreamSource(stream);
-		var evt = new CustomEvent('sourceCap', {bubbles: true, detail: source});
-		fset.dispatchEvent(evt);
-	    })
-	}
-	if(this.name == 'play'){
-
-	    if(window.source.mediaElement) {
-
-		window.source.connect(window.synth);
-		synth.connect(master.destination);
-		window.source.mediaElement.play();
-
-	    }
-	    else if(window.source.noteOn) window.source.noteOn(0)
-	    else{
-		var sine = master.createOscillator();
-//		sine.connect(window.source);
-		window.source.connect(window.synth);
-		window.synth.connect(master.destination);
-		sine.noteOn(0);
-	    }
-	}
-
-    }
 }
 
 function makeStyle(str){
